1072ee9114899ed5de5f2495c5966128ed842069
saeed.butt <saeed.butt@n-fuse.co>
2019-04-05 12:01:38 +0200

    up start

--- start.ts
+++ start.ts
@@ -1,13 +1,9 @@
-'use strict';
+import * as sconfig from '@restorecommerce/service-config';
+import * as Cluster from '@restorecommerce/cluster-service';
 
-import * as chassis from '@restorecommerce/chassis-srv';
-import * as  Cluster from '@restorecommerce/cluster-service';
-import * as co from 'co';
-
-co(async function startCluster(): Promise<any> {
-  const cfg: any = await co(chassis.config.get());
-  const server: any = new Cluster(cfg);
-  server.run('./worker.js');
-}).catch((err) => {
-  console.error(err);
+const cfg = sconfig(process.cwd());
+const server = new Cluster(cfg);
+server.run('./service');
+process.on('SIGINT', () => {
+  server.stop();
 });
