664c4f8d4c612e012654dd7a748fb7f21ef1b351
saeed.butt <saeed.butt@n-fuse.co>
2019-04-05 12:06:33 +0200

    fix start

--- start.ts
+++ start.ts
@@ -3,7 +3,7 @@ import * as Cluster from '@restorecommerce/cluster-service';
 
 const cfg = sconfig(process.cwd());
 const server = new Cluster(cfg);
-server.run('./service');
+server.run('./worker.js');
 process.on('SIGINT', () => {
   server.stop();
 });
