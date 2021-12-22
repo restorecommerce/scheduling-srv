282e1c1973bc8a4c5a45ad3de84b843ce5c8c61c
akumar <arun.kumar@n-fuse.co>
2021-12-22 12:29:40 +0100

    fix: set error instead of number in rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     ],
     "rules": {
       "@typescript-eslint/adjacent-overload-signatures": "error",
-      "@typescript-eslint/no-floating-promises": 2,
+      "@typescript-eslint/no-floating-promises": "error",
       "@typescript-eslint/naming-convention": [
         "error",
         {
