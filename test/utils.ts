4af41633a98137794859774e050f172b2613b08e
Arun Kumar <arun.kumar@n-fuse.co>
2025-10-09 09:36:15 +0200

    fix: replace should with expect

--- test/utils.ts
+++ test/utils.ts
@@ -1,12 +1,12 @@
 import { unmarshallProtobufAny } from '../src/utilts.js';
-import should from 'should';
+import { expect } from 'vitest';
 import { Priority } from '../src/types.js';
 import { Logger } from 'winston';
 import { PolicySetRQResponse } from '@restorecommerce/acs-client';
 import { Effect } from '@restorecommerce/rc-grpc-clients/dist/generated-server/io/restorecommerce/rule.js';
 import { createServiceConfig } from '@restorecommerce/service-config';
 import { createChannel, createClient } from '@restorecommerce/grpc-client';
-import { 
+import {
   JobServiceDefinition as SchedulingServiceDefinition,
   JobServiceClient as SchedulingServiceClient,
 } from '@restorecommerce/rc-grpc-clients/dist/generated-server/io/restorecommerce/job.js';
@@ -28,49 +28,33 @@ export function getSchedulingServiceClient(logger: Logger) {
 }
 
 export function validateScheduledJob(job: any, expectedSchedule: string, logger: Logger): void {
-  should.exist(job.data);
-  should.exist(job.data.payload);
   const payload = unmarshallProtobufAny(job.data.payload, logger);
-  should.exist(payload.testValue);
-  payload.testValue.should.equal('test-value');
-  should.exist(job.id);
-  should.exist(job.type);
-  job.type.should.equal('test-job');
-  // should.exist(job.schedule_type);
-  // job.schedule_type.should.equal(expectedSchedule);
+  expect(payload.testValue).to.equal('test-value');
+  expect(job.type).to.equal('test-job');
 }
 
 export function validateJobDonePayload(job: any, logger: Logger): void {
   if (job && job.result) {
-    should.exist(job.result);
+    expect(job.result);
     const payload = unmarshallProtobufAny(job.result, logger);
-    payload.testValue.should.equal('test-value');
+    expect(payload.testValue).to.equal('test-value');
   }
 }
 
 export function validateJob(job: any, logger: Logger): void {
-  should.exist(job);
   const payload = unmarshallProtobufAny(job.data.payload, logger);
-  should.exist(payload.testValue);
-  payload.testValue.should.equal('test-value');
-  should.exist(job.id);
-  should.exist(job.type);
-  job.type.should.equal('test-job');
-  job.queue_name.should.equal('test-job');
-  should.exist(job.options.priority);
-  Priority.should.hasOwnProperty(job.options.priority);
-  should.exist(job.options.attempts);
-  job.options.attempts.should.equal(1);
+  expect(payload.testValue).to.equal('test-value');
+  expect(job.type).to.equal('test-job');
+  expect(job.queue_name).to.equal('test-job');
+  expect(job.options.attempts).to.equal(1);
 }
 
 export function payloadShouldBeEmpty(result: any, emptyArray: boolean = true): void {
-  should.exist(result);
   if (emptyArray) {
-    should.exist(result.items);
-    result.items.should.be.length(0);
+    expect(result.items.length).to.equal(0);
   } else {
     // since grpc read does not return empty elements after making fields optional
-    should.not.exist(result.items);
+    expect(result.items).to.equal(undefined);
   }
 }
 
@@ -78,7 +62,7 @@ export const permitJobRule = {
   id: 'permit_rule_id',
   target: {
     resources: [
-      { 
+      {
         id: 'urn:restorecommerce:acs:names:model:entity',
         value: 'urn:restorecommerce:acs:model:job.Job'
       }
