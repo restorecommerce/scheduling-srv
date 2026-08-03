b1e9875c0635ee77b34122a18e1a36e4344edd1b
Gerald Baulig <gerald.baulig@n-fuse.co>
2026-08-03 16:43:21 +0200

    refactor(jobs): experimental default job

--- src/external-jobs/default_queue_jobs.ts
+++ src/external-jobs/default_queue_jobs.ts
@@ -3,9 +3,16 @@ import {
 } from '@restorecommerce/scs-jobs';
 
 const main: DefaultExportFunc = async (cfg, logger, events, runWorker) => {
+  const arangoDb = await create(cfg.get('arangoDb'), logger);
   await runWorker('default-queue', 1, cfg, logger, events as any, async (job: any) => {
     // depending on job type add implementation here for Jobs to be run on default-queue
   });
 };
 
-export default main;
+export default {
+  main
+};
+
+import {
+  create
+} from '@restorecommerce/chassis-srv/lib/database/provider/arango/index.js';
