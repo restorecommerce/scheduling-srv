0830e5d5243890bdf17812604b0afa81d4cb62ee
Gerald Baulig <gerald.baulig@n-fuse.co>
2026-08-10 12:26:19 +0200

    test(default-job): add debug logs to default job for demonstration

--- src/external-jobs/default_queue_jobs.ts
+++ src/external-jobs/default_queue_jobs.ts
@@ -5,8 +5,10 @@ import {
 } from '@restorecommerce/scs-jobs';
 
 export const main: DefaultExportFunc = async (cfg, logger, events, runWorker) => {
+  logger?.debug('Default Job Funktion found and loaded.');
   await runWorker('default-queue', 1, cfg, logger, events as any, async (job: any) => {
     // depending on job type add implementation here for Jobs to be run on default-queue
+    logger?.debug('Default Job Funktion triggered and executed.');
   });
 };
 
